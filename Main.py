4eadd9378a4291c2ea83ce95c33e89768f185e5f
sjnosukequy <21110795@student.hcmute.edu.vn>
2023-09-30 21:36:33 +0700

    start

--- Main.py
+++ Main.py
@@ -4,6 +4,7 @@ from Scripts.Buttons import Button
 class Game:
     def __init__(self):
         self.state = "Main_Menu"
+        self.block_size = 32
         self.Butt_Play = Button((screen_w/2 - 100, 250), (200, 50), 'red', 'Play', 'Play', text_color='white' ,text_size= 35)
         self.Butt_Exit = Button((screen_w/2 - 100, 320), (200, 50), 'red', 'Quit', 'Quit', text_color='white' ,text_size= 35)
     
@@ -27,8 +28,8 @@ class Game:
 
 if __name__ == '__main__':
     pygame.init()
-    screen_w = 900
-    screen_h = 400
+    screen_w = 1280
+    screen_h = 736
     screen = pygame.display.set_mode((screen_w, screen_h))
     display = pygame.surface.Surface((screen_w, screen_h))
     clock = pygame.time.Clock()
@@ -44,5 +45,5 @@ if __name__ == '__main__':
                 sys.exit()
         
         game.run()
-        screen.blit(display, (0,0))
+        screen.blit(pygame.transform.scale(display, (screen_w, screen_h)), (0,0))
         pygame.display.flip()
