1a3e1751c86eda2f102218663411cc30993e97e8
Đỗ Quang Triệu <116730740+TrieuDoQuang@users.noreply.github.com>
2023-10-13 22:36:17 +0700

    particles

--- Main.py
+++ Main.py
@@ -1,21 +1,29 @@
-import pygame, sys
+import pygame
+import sys
+import random
+import math
 from Scripts.Buttons import Button
 from Scripts.Clouds import Clouds
 from Scripts.Player import Player
 from Scripts.Assets import *
 from Scripts.Tilemap import Tilemap
+from Scripts.Particles import Particles
+
 
 class Game:
     def __init__(self):
         self.assets = Assets
         self.state = "Main_Menu"
         self.block_size = 32
-        self.scroll = [0,0]
-        self.Clouds = Clouds(self.assets['Clouds'], count=20, size_mul = 2)
-        self.Player = Player('Player', (250, 100), (25, 64), self.assets, size_mul= 1, animoffset=(-32, 0))
-        self.Butt_Play = Button((screen_w/2 - 100, 250), (200, 50), 'red', 'Play', 'Play', text_color='white' ,text_size= 35)
-        self.Butt_Exit = Button((screen_w/2 - 100, 320), (200, 50), 'red', 'Quit', 'Quit', text_color='white' ,text_size= 35)
-        self.tilemap = Tilemap(self, tile_size = 32)
+        self.scroll = [0, 0]
+        self.Clouds = Clouds(self.assets['Clouds'], count=20, size_mul=2)
+        self.Player = Player('Player', (250, 100), (25, 64),
+                             self.assets, size_mul=1, animoffset=(-32, 0))
+        self.Butt_Play = Button((screen_w/2 - 100, 250), (200, 50),
+                                'red', 'Play', 'Play', text_color='white', text_size=35)
+        self.Butt_Exit = Button((screen_w/2 - 100, 320), (200, 50),
+                                'red', 'Quit', 'Quit', text_color='white', text_size=35)
+        self.tilemap = Tilemap(self, tile_size=32)
         self.transition = -30
         dest = 'Data/Maps/' + str(level) + '.json'
         try:
@@ -23,6 +31,14 @@ class Game:
         except FileNotFoundError:
             pass
         self.temp = 1
+
+        # Particles leaf
+        self.Particles = []
+
+        self.Leaf_spawner = []
+        for tree in self.tilemap.extract([('large_decor', 2)], True):
+            self.Leaf_spawner.append(pygame.Rect(
+                tree['pos'][0] + 4, tree['pos'][1] + 4, 23, 13))
 
     def run(self):
         if self.state == "Main_Menu":
@@ -36,29 +52,51 @@ class Game:
             self.Butt_Exit.render(display)
 
         elif self.state == "Play":
-            #WIN_CONDITION
+            # WIN_CONDITION
             # if not len(self.enemies) and not len(self.Boss):
             #     global level
             #     global max_level
             #     level = min(level + 1, max_level)
             #     self.End = True
 
-            #BACK_GROUND
-            display.blit(pygame.transform.scale(self.assets['BG'], (screen_w, screen_h)), (0,0))
+            # BACK_GROUND
+            display.blit(pygame.transform.scale(
+                self.assets['BG'], (screen_w, screen_h)), (0, 0))
 
-            ##RENDER SCROLL
-            self.scroll[0] += (self.Player.rect().centerx - display.get_width()/2 - self.scroll[0]) / 30 # type: ignore
-            self.scroll[1] += (self.Player.rect().centery - display.get_height()/2 - self.scroll[1]) / 20 # type: ignore
+            # Particles leaf random
+            for rect in self.Leaf_spawner:
+                if random.random() * 49000 < rect.width * rect.height:
+                    pos = (rect.x + random.random() * rect.width,
+                           rect.y + random.random() * rect.height)
+                    self.Particles.append(Particles(
+                        self.assets, 'leaf', pos, velocity=[-0.1, 0.3], frame=random.randint(0, 20)))
+            #
+
+            # RENDER SCROLL
+            self.scroll[0] += (self.Player.rect().centerx -
+                               display.get_width()/2 - self.scroll[0]) / 30  # type: ignore
+            self.scroll[1] += (self.Player.rect().centery -
+                               display.get_height()/2 - self.scroll[1]) / 20  # type: ignore
             render_scroll = (int(self.scroll[0]), int(self.scroll[1]))
 
-            #RENDERS
-            self.Clouds.render(display, offset = render_scroll)
+            # RENDERS
+            self.Clouds.render(display, offset=render_scroll)
             self.Clouds.update()
-            self.tilemap.render(display, offset = render_scroll)
+            self.tilemap.render(display, offset=render_scroll)
             self.Player.update(self.tilemap)
-            self.Player.render(display, offset= render_scroll)
+            self.Player.render(display, offset=render_scroll)
 
-            #TRANSITION
+            # RENDERS PARTICLES LEAF
+            for Particle in self.Particles.copy():
+                kill = Particle.update()
+                if Particle.type == 'leaf':
+                    Particle.pos[0] += math.sin(
+                        Particle.animation.frame * (math.pi / 360)) * 0.3
+                Particle.render(display, offset=render_scroll)
+                if kill:
+                    self.Particles.remove(Particle)
+
+            # TRANSITION
             if not self.temp:
                 self.transition += 1
             if self.transition < 0:
@@ -66,13 +104,15 @@ class Game:
 
             if self.transition:
                 trans_surf = pygame.Surface(display.get_size())
-                pygame.draw.circle(trans_surf, "white", (display.get_width() /2, display.get_height() /2), (30 - abs(self.transition)) * 25 )
+                pygame.draw.circle(trans_surf, "white", (display.get_width(
+                ) / 2, display.get_height() / 2), (30 - abs(self.transition)) * 25)
                 trans_surf.set_colorkey("white")
-                display.blit(trans_surf, (0,0))
+                display.blit(trans_surf, (0, 0))
 
         elif self.state == "Quit":
             pygame.quit()
             sys.exit()
+
 
 if __name__ == '__main__':
     pygame.init()
@@ -81,7 +121,7 @@ if __name__ == '__main__':
     clock = pygame.time.Clock()
     level = 0
 
-    FPS = 60  #FRAMERATE LIMITER
+    FPS = 60  # FRAMERATE LIMITER
     game = Game()
 
     while True:
@@ -90,7 +130,8 @@ if __name__ == '__main__':
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
-        
+
         game.run()
-        screen.blit(pygame.transform.scale(display, (screen_w, screen_h)), (0,0))
+        screen.blit(pygame.transform.scale(
+            display, (screen_w, screen_h)), (0, 0))
         pygame.display.flip()
--- Scripts/Assets.py
+++ Scripts/Assets.py
@@ -3,26 +3,29 @@ import pygame
 pygame.init()
 screen_w = 1280
 screen_h = 736
-screen = pygame.display.set_mode((screen_w,screen_h))
+screen = pygame.display.set_mode((screen_w, screen_h))
 
-Assets= {
-    'BG' : Load_IMG('bg/Background_Day.png'),
-    'Clouds' : Load_IMGS('clouds'),
+Assets = {
+    'BG': Load_IMG('bg/Background_Day.png'),
+    'Clouds': Load_IMGS('clouds'),
     'stone': [Load_IMGS('tiles/stone'), 2],
     'dirt': [Load_IMGS('tiles/dirt'), 2],
-    'grass': [Load_IMGS ('tiles/grass'), 2],
-    'decor' : [Load_IMGS('tiles/decor'), 2],
-    'large_decor' : [Load_IMGS('tiles/large_decor'), 3],
-    #PLAYER ANIMS
-    'Player/idle': Animation(Load_IMGS('entities/player/idle'), dur= 13),
-    'Player/jump': Animation(Load_IMGS('entities/player/jump'), dur= 13, loop= False),
+    'grass': [Load_IMGS('tiles/grass'), 2],
+    'decor': [Load_IMGS('tiles/decor'), 2],
+    'large_decor': [Load_IMGS('tiles/large_decor'), 3],
+    # Particles
+    'Particles/leaf': Animation(Load_IMGS('particles/leaf'), dur=10, loop=False),
+    'Particles/particle': Animation(Load_IMGS('particles/particle'), dur=10, loop=False),
+    # PLAYER ANIMS
+    'Player/idle': Animation(Load_IMGS('entities/player/idle'), dur=13),
+    'Player/jump': Animation(Load_IMGS('entities/player/jump'), dur=13, loop=False),
     'Player/run': Animation(Load_IMGS('entities/player/run')),
-    'Player/wall_slide': Animation(Load_IMGS('entities/player/wall_slide')), 
-    'Player/dash': Animation(Load_IMGS('entities/player/dash'), dur= 5, loop= False),
-    'Player/shield': Animation(Load_IMGS('entities/player/shield'), dur= 10, loop= False),
-    'Player/hurt': Animation(Load_IMGS('entities/player/hurt'), dur= 13, loop= False), 
-    'Player/fall': Animation(Load_IMGS('entities/player/fall'), dur= 10), 
-    'Player/die': Animation(Load_IMGS('entities/player/die'), dur= 13, loop= False),
-    'Player/attack0': Animation(Load_IMGS('entities/player/attack1'), dur= 7, loop= False), 
-    'Player/attack1': Animation(Load_IMGS('entities/player/attack2'), dur= 8, loop= False), 
+    'Player/wall_slide': Animation(Load_IMGS('entities/player/wall_slide')),
+    'Player/dash': Animation(Load_IMGS('entities/player/dash'), dur=5, loop=False),
+    'Player/shield': Animation(Load_IMGS('entities/player/shield'), dur=10, loop=False),
+    'Player/hurt': Animation(Load_IMGS('entities/player/hurt'), dur=13, loop=False),
+    'Player/fall': Animation(Load_IMGS('entities/player/fall'), dur=10),
+    'Player/die': Animation(Load_IMGS('entities/player/die'), dur=13, loop=False),
+    'Player/attack0': Animation(Load_IMGS('entities/player/attack1'), dur=7, loop=False),
+    'Player/attack1': Animation(Load_IMGS('entities/player/attack2'), dur=8, loop=False),
 }
